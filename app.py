bd0553606cabbaf4f150a9bd74fed976d964d012
ThibautNguyen <thibaut.nguyen@spallian.com>
2025-04-09 18:10:38 +0200

    Initial minimal app

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Test de déploiement Streamlit")
+st.write("Si vous voyez cette page, le déploiement fonctionne !")
+st.markdown("---")
+st.write("Application de gestion des métadonnées")
